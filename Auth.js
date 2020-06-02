c41f12679459af60b3c49abcfd43d18bf70f85be
pjonp <pjonp01@gmail.com>
2020-06-02 02:15:25 -0400

    Update Auth.js

--- Auth.js
+++ Auth.js
@@ -75,7 +75,7 @@ module.exports = function(app, db) {
   passport.use(new DiscordStrategy({
       clientID: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
-      callbackURL: process.env.DISCORD_CLIENT_CALLBACK,
+      callbackURL: process.env.DISCORD_CALLBACK,
       scope: ['identify'],
       passReqToCallback: true
     },
